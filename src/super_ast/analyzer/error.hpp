c03ce4e4d39aac48f27049519747d874071c0bdb
Miquel Llobet <mllobet.cm@gmail.com>
2015-06-01 23:37:04 +0200

    Added Error class

---
+++ src/super_ast/analyzer/error.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Error {
+    public:
+        int line_number;
+        std::string function;
+        std::string short_desc;
+        std::string long_desc;
+
+        Error(int ln, std::string f, std::string s, std::string l) : line_number(ln), function(f), short_desc(s), long_desc(l) {
+        }
+
+    private:
+
+};
