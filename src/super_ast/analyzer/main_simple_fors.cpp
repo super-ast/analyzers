0afd0680103ba8ee96b313b5754cdc6ad5acc355
Albert Martinez <albertmg7@gmail.com>
2015-06-02 03:58:18 +0200

    added forgotten main_simple_fors

---
+++ src/super_ast/analyzer/main_simple_fors.cpp
@@ -0,0 +1,18 @@
+#include "simple_fors.hpp"
+
+int main() {
+  const super_ast::Block* ast = super_ast::Parse(std::cin);
+
+  super_ast::SimpleFors simple_fors;
+  ast->Accept(simple_fors);
+
+  std::vector<Error> errors = simple_fors.GetErrors();
+  for (const Error& error : errors) {
+    std::cout << "line:" << error.line_number << 
+        " - function: " << error.function <<
+        " - short description: " << error.short_desc <<
+        " - long description: " << error.long_desc << std::endl;
+  }
+
+  return 0;
+}
